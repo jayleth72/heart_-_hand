abc1c263cb6751313c27e541c32250b27731aa29
Jay Letheby <jay.letheby@gmail.com>
2018-04-28 15:35:02 +1000

    go live settings 6

--- dev_settings.py
+++ dev_settings.py
@@ -1,5 +1,5 @@
 # DATABASE_PASSWORD = 'postgresql://postgres:Lickmysmoothballs69$@localhost/hearthand'
-DATABASE_PASSWORD = 'postgres://wktgtfvpbjczbv:4889621e197f5b9aa0a8b2211bed3c200febd4dbdb4589a0f6ebd5ff46d48991@ec2-54-83-1-94.compute-1.amazonaws.com:5432/?sslmode=require'
+DATABASE_PASSWORD = 'postgres://wktgtfvpbjczbv:4889621e197f5b9aa0a8b2211bed3c200febd4dbdb4589a0f6ebd5ff46d48991@ec2-54-83-1-94.compute-1.amazonaws.com:5432/d5ob089dcc843s?sslmode=require'
 SECRET_KEY = '\x95\xca\xb7\xde\xda-\x8c\xffIpTc\x9c-\x14\xaa\xbej\xf0\xd3B\x08E'
 SECURITY_PASSWORD_HASH = 'bcrypt'
 SECURITY_PASSWORD_SALT = '\xb2\x8b\xe4\x99\xc5\xa2\x8dA\x91.qd\x94LDd\xc6/s\xef\x18S\xbd^'
