fa3b2621e59c969d353ecf8867ce1557861c9528
Jay Letheby <jay.letheby@gmail.com>
2018-04-29 18:53:14 +1000

    set up live1

--- app.py
+++ app.py
@@ -3,12 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 from flask import render_template, request, url_for, redirect, session
 from flask_security import Security, SQLAlchemyUserDatastore, UserMixin, RoleMixin, login_required
 from flask.ext.security.utils import encrypt_password
-from boto.s3.connection import S3Connection
 import os
 # from settings import *
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']= os.environ.get('DATABASE_URL').append("?sslmode=require")
+app.config['SQLALCHEMY_DATABASE_URI']= os.environ.get('DATABASE_URL') + "?sslmode=require"
 app.config['SECRET_KEY']= os.environ.get('SECRET_KEY') 
 app.config['SECURITY_REGISTERABLE']= os.environ.get('SECURITY_REGISTERABLE') 
 app.config['SECURITY_PASSWORD_HASH'] = os.environ.get('SECURITY_PASSWORD_HASH')  
