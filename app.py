f6ef6c9c7fcd5eded3943e9ab3b4c3b706816983
Jay Letheby <jay.letheby@gmail.com>
2018-05-05 21:02:31 +1000

    added customer table

--- app.py
+++ app.py
@@ -15,23 +15,44 @@ app.config['SECURITY_PASSWORD_SALT'] = os.environ.get('SECURITY_PASSWORD_SALT')
 db=SQLAlchemy(app)
 
 # Define models
-roles_users = db.Table('roles_users',
-        db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
-        db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
+# roles_users = db.Table('roles_users',
+#         db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+#         db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
 
-class Role(db.Model, RoleMixin):
+# class Role(db.Model, RoleMixin):
+#     id = db.Column(db.Integer(), primary_key=True)
+#     name = db.Column(db.String(80), unique=True)
+#     description = db.Column(db.String(255))
+
+# class User(db.Model, UserMixin):
+#     id = db.Column(db.Integer, primary_key=True)
+#     email = db.Column(db.String(255), unique=True)
+#     password = db.Column(db.String(255))
+#     active = db.Column(db.Boolean())
+#     confirmed_at = db.Column(db.DateTime())
+#     roles = db.relationship('Role', secondary=roles_users,
+#                             backref=db.backref('users', lazy='dynamic'))
+
+class Customer(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
-    name = db.Column(db.String(80), unique=True)
-    description = db.Column(db.String(255))
-
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(80))
+    last_name = db.Column(db.String(80)
     email = db.Column(db.String(255), unique=True)
-    password = db.Column(db.String(255))
-    active = db.Column(db.Boolean())
-    confirmed_at = db.Column(db.DateTime())
-    roles = db.relationship('Role', secondary=roles_users,
-                            backref=db.backref('users', lazy='dynamic'))
+    street_address = db.Column(db.String(150))
+    suburb = db.Column(db.String(50))
+    state = db.Column(db.String(50))
+    postcode = db.Column(db.String(50))
+    phone = db.Column(db.String(50))
+    alternative_contact = db.Column(db.String(100))
+    alternative_contact_phone = db.Column(db.String(50))
+    notes = db.Column(db.String(255))                            
+
+class Child(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    parent_id = db.Column(db.Integer(), db.ForeignKey('customer.id'))
+    first_name = db.Column(db.String(80))
+    last_name = db.Column(db.String(80))
+    notes= db.Column(db.String(255)) 
 
 # Setup Flask-Security
 user_datastore = SQLAlchemyUserDatastore(db, User, Role)
