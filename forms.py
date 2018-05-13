ecb4c4e286e872ca0c7ef8d88010a16159dcad20
Jay Letheby <jay.letheby@gmail.com>
2018-05-13 22:59:42 +1000

    added field render macro

--- forms.py
+++ forms.py
@@ -2,16 +2,16 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, SelectField
 
 class CustomerEntryForm(FlaskForm):
-    first_name = StringField('first_name')
-    last_name = StringField('last_name')
-    email = StringField('email')
-    street_address = StringField('street_address')
-    suburb = StringField('suburb')
+    first_name = StringField('First Name', validators=[DataRequired())
+    last_name = StringField('Last Name', validators=[DataRequired())
+    email = StringField('Email', validators=[DataRequired())
+    street_address = StringField('Street Address', validators=[DataRequired())
+    suburb = StringField('Suburb', validators=[DataRequired())
     state = SelectField(u'State', choices=[('QLD', 'Queensland'), ('NT', 'Northern Territory'), ('WA', 'Western Australia'), ('NSW', 'New South Wales'), ('Vic', 'Victoria'), ('Tas', 'Tasmania'), ('SA', 'South Australia')])
-    postcode = IntegerField('postcode')
-    phone = StringField('phone')
-    alternative_contact = StringField('alternative_contact')
-    alternative_contact_phone = StringField('alternative_contact_phone')
-    notes = StringField('notes')
+    postcode = IntegerField('Postcode', validators=[DataRequired())
+    phone = StringField('Phone', validators=[DataRequired())
+    alternative_contact = StringField('Alternative Contact')
+    alternative_contact_phone = StringField('Alternative Contact Phone')
+    notes = StringField('Notes')
 
     
