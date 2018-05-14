6ee71df44e3ddf94a88dc0d33515686f3e72c03a
Jay Letheby <jay.letheby@gmail.com>
2018-05-14 14:40:08 +1000

    changed macros and added erros

--- forms.py
+++ forms.py
@@ -1,16 +1,16 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, SelectField
-from wtforms.validators import DataRequired
+from wtforms.validators import InputRequired
 
 class CustomerEntryForm(FlaskForm):
-    first_name = StringField('First Name', validators=[DataRequired()])
-    last_name = StringField('Last Name', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired()])
-    street_address = StringField('Street Address', validators=[DataRequired()])
-    suburb = StringField('Suburb', validators=[DataRequired()])
+    first_name = StringField('First Name', validators=[InputRequired(message='First name is required')])
+    last_name = StringField('Last Name', validators=[InputRequired(message='Last name is required')])
+    email = StringField('Email', validators=[InputRequired(message='Email is required')])
+    street_address = StringField('Street Address', validators=[InputRequired(message='Street address is required')])
+    suburb = StringField('Suburb', validators=[InputRequired(message='Suburb is required')])
     state = SelectField(u'State', choices=[('QLD', 'Queensland'), ('NT', 'Northern Territory'), ('WA', 'Western Australia'), ('NSW', 'New South Wales'), ('Vic', 'Victoria'), ('Tas', 'Tasmania'), ('SA', 'South Australia')])
-    postcode = IntegerField('Postcode', validators=[DataRequired()])
-    phone = StringField('Phone', validators=[DataRequired()])
+    postcode = IntegerField('Postcode', validators=[InputRequired(message='Postcode is required')])
+    phone = StringField('Phone', validators=[InputRequired(message='Phone No. is required')])
     alternative_contact = StringField('Alternative Contact')
     alternative_contact_phone = StringField('Alternative Contact Phone')
     notes = StringField('Notes')
