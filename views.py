7c12d5df12f88b49cf056207deef7060d6b6b19f
Jay Letheby <jay.letheby@gmail.com>
2018-05-17 12:33:16 +1000

    customer entry function

--- views.py
+++ views.py
@@ -1,4 +1,4 @@
-from app import app
+from app import app, db
 from flask_security import login_required
 from flask import render_template, redirect, url_for, request
 from flask_wtf import FlaskForm
