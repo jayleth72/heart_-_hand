2682ff06a3ccfeb6c86cb90cac7db45f9018813f
Jay Letheby <jay.letheby@gmail.com>
2018-05-18 21:24:12 +1000

    added email field

--- forms.py
+++ forms.py
@@ -1,11 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, SelectField, TextAreaField
+from wtforms.fields.html5 import EmailField
 from wtforms.validators import InputRequired
 
 class CustomerEntryForm(FlaskForm):
     first_name = StringField('First Name', validators=[InputRequired(message='First name is required')])
     last_name = StringField('Last Name', validators=[InputRequired(message='Last name is required')])
-    email = StringField('Email', validators=[InputRequired(message='Email is required')])
+    email = EmailField('Email', validators=[InputRequired(message='Email is required')])
     street_address = StringField('Street Address', validators=[InputRequired(message='Street address is required')])
     suburb = StringField('Suburb', validators=[InputRequired(message='Suburb is required')])
     state = SelectField(u'State', choices=[('QLD', 'Queensland'), ('NT', 'Northern Territory'), ('WA', 'Western Australia'), ('NSW', 'New South Wales'), ('Vic', 'Victoria'), ('Tas', 'Tasmania'), ('SA', 'South Australia')])
