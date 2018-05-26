43fc3926c94d6ce3ea09fd7367acf2ea46151dca
Jay Letheby <jay.letheby@gmail.com>
2018-05-26 17:10:03 +1000

    fixing child form

--- views.py
+++ views.py
@@ -28,69 +28,47 @@ def index():
 @app.route('/add_customer', methods=['GET','POST'])
 @login_required
 def add_customer():
-    form = CustomerEntryForm()
+    formData = CustomerEntryForm()
 
     if request.method == 'POST':
-        if form.validate():
+        if formData.validate():
             customer = Customer(first_name=request.form['first_name'],last_name=request.form['last_name'],email=request.form['email'],street_address=request.form['street_address']
                        ,suburb=request.form['suburb'],state=request.form['state'],postcode=request.form['postcode'],phone=request.form['phone']
                        ,alternative_contact=request.form['alternative_contact'],alternative_contact_phone=request.form['alternative_contact_phone'],notes=request.form['notes'])
-            form.populate_obj(customer)
+            formData.populate_obj(customer)
             
             db.session.add(customer)
             db.session.commit()
             flash('New customer was successfully added')
-            return redirect(url_for('add_child', first_name=request.form['first_name'], last_name=request.form['last_name'] ,email=request.form['email'] ))
+            return add_child(formData)
         else:
             flash("Your form contained errors")
             return redirect(url_for('add_customer'))
     elif request.method == 'GET': 
-        return render_template('add_customer.html', form=form)    
+        return render_template('add_customer.html', form=formData)    
 
 
-@app.route('/add_child/<string:first_name>/<string:last_name>/<string:email>', methods=['GET','POST'])
+@app.route('/add_child', methods=['GET','POST'])
 @login_required
-def add_child(first_name, last_name, email):
+def add_child(formData):
     # get customer id for insertion as foreign key in child table
-    # customerId = Customer.query.filter_by(email=email).first()
+    customerId = Customer.query.filter_by(email=formData.data['email']).first()
 
     form = ChildEntryForm()
-
-    # if request.method == 'POST':
-    #     if form.validate():
-    #         child = Child(parent_id=customerId,first_name=request.form['first_name'],last_name=request.form['last_name'],date_of_birth=request.form['date_of_birth'],notes=request.form['notes'])
-    #         form.populate_obj(child)
-            
-    #         db.session.add(child)
-    #         db.session.commit()
-    #         flash('New child was successfully added')
-    #         return render_template('success.html')  
-    #     else:
-    #         flash("Your form contained errors")
-    #         return render_template('success.html') 
-    # elif request.method == 'GET': 
-    return render_template('/add_child.html', form=form, first_name=first_name, last_name=last_name, email=email)  
-
-@app.route('/add_childs', methods=['GET','POST'])
-@login_required
-def add_childs():
-    # get customer id for insertion as foreign key in child table
-    form = ChildEntryForm()
-    customerId = request.form['email']
 
     if request.method == 'POST':
-        if form.validate():
-            child = Child(parent_id=customerId,first_name=request.form['first_name'],last_name=request.form['last_name'],date_of_birth=request.form['date_of_birth'],notes=request.form['notes'])
-            form.populate_obj(child)
-            
-            db.session.add(child)
-            db.session.commit()
-            flash('New child was successfully added')
-            return render_template('success.html')  
-        else:
-            flash("Your form contained errors")
-            return render_template('success.html') 
-       
+         if form.validate():
+             child = Child(parent_id=customerId,first_name=request.form['first_name'],last_name=request.form['last_name'],date_of_birth=request.form['date_of_birth'],notes=request.form['notes'])
+             form.populate_obj(child)
+             db.session.add(child)
+             db.session.commit()
+             flash('New child was successfully added')
+             return render_template('success.html')  
+         else:
+             flash("Your form contained errors")
+             return render_template('success.html') 
+    elif request.method == 'GET': 
+             return render_template('/add_child.html')  
 
 
 @app.route('/search_customers', methods=['GET','POST'])
