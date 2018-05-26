633600e040cb28c8ce425d433fde4dcf348503b1
Jay Letheby <jay.letheby@gmail.com>
2018-05-26 22:02:33 +1000

    fixing child form

--- views.py
+++ views.py
@@ -69,7 +69,7 @@ def add_child(customer):
              flash("Your form contained errors")
              return redirect(url_for('add_child', customer=customer))
     
-    return render_template('/add_child.html',  form=form, customer=customer,)  
+    return render_template('/add_child.html',  **locals())  
 
 
 @app.route('/search_customers', methods=['GET','POST'])
